4f526d99a80a669d15e7df7d748800d122f34e32
Nikolay Podstrigan <parikmaher534@gmail.com>
2015-02-05 00:54:16 +0300

    Global remove of temp files

--- public/frontend/.bem/cache/desktop.bundles/index/index.styl.css~styl.meta.js
+++
@@ -1,1 +0,0 @@
-{"buildFiles":[{"file":"page_theme_islands.styl","absPath":"/Users/parikmaher/Projects/YandexDisk/WantEat/public/frontend/libs/bem-components/design/common.blocks/page/_theme/page_theme_islands.styl","lastUpdated":1422042600000,"suffix":"styl"}]}
