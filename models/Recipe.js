ddbe56c4ec7b39f6c5e8f2b6bc5239ab3d423486
Nikolay Podstrigan <parikmaher534@gmail.com>
2015-02-22 00:52:54 +0300

    Update recipe model

--- models/Recipe.js
+++ models/Recipe.js
@@ -2,18 +2,20 @@ var Recipe,
     mongoose = require('mongoose');
 
 RecipeSchema = mongoose.Schema({
-    name: { type: String, trim: true },
+    name: { type: String, trim: true, index: 1 },
     description: { type: String, trim: true },
     ingredients: [
-        { type: mongoose.Schema.Types.ObjectId, ref: 'RecipeIngredient'}
+        { type: mongoose.Schema.Types.ObjectId, ref: 'RecipeIngredient' }
     ],
     time: {type: String },
     photo: { type: String, trim: true },
     content: { type: String, trim: true },
     totalCalories: { type: Number },
     linkToOrigin: { type: String },
-    search: [{ type: String, trim: true }]
+    search: [{ type: String, trim: true, index: true }]
 });
+
+RecipeSchema.index({ search: 1 });
 
 module.exports = {
     name: 'Recipe',
